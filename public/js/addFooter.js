dca70080c41dbac963cd3505f936447c400ad857
Pappim Pipatkasrira <printtopipatkasira@gmail.com>
2018-10-03 17:00:34 +0700

    Started adding views to back-end with seperate the header and footer part as JS

---
+++ public/js/addFooter.js
@@ -0,0 +1,90 @@
+$(document).ready(function() {
+
+  $('.footer_area').html(`
+    <div class="footer_top_area text-center">
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <div class="logo">
+              <a href="index.html"><img src="img/logo web v1.png" alt="logo" height="10%" width="10%"></a>
+              <p></p>
+              <img class="devider" src="img/footer_devider.png" alt="">
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-4 col-md-4">
+            <div class="single_contact">
+              <a href="#"><i class="fa fa-phone"></i></a>
+              <p>012-345-6789</p>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-4">
+            <div class="single_contact">
+              <a href="#"><i class="fa fa-map-marker"></i></a>
+              <p>2033 Phahonyothin Alley, Khwaeng Lat Yao, Khet Chatuchak, Krung Thep Maha Nakhon 10900.</p>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-4">
+            <div class="single_contact">
+              <a href="#"><i class="fa fa-envelope-o"></i></a>
+              <a href="#">
+                <p>kk@kk.com</p>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="footer_middle_area">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-3 col-md-6 col-sm-6">
+            <div class="footer_widget">
+              <h4>company</h4>
+              <ul class="footer_menu">
+                <li><a href="#">about us</a></li>
+                <li><a href="#">terms &amp; conditions</a></li>
+                <li><a href="#">contact us</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6 col-sm-6">
+            <div class="footer_widget">
+              <h4>customer service</h4>
+              <ul class="footer_menu">
+                <li><a href="#">help &amp; faq</a></li>
+                <li><a href="#">shipping &amp; policies</a></li>
+                <li><a href="#">store locations</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-lg-2 col-md-6 col-sm-6">
+            <div class="footer_widget">
+              <h4>my account</h4>
+              <ul class="footer_menu">
+                <li><a href="#">my orders</a></li>
+                <li><a href="#">my personal info</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="footer_bottom_area">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <div class="copyright">
+              <p>Copyright ©2018 <a href="#"> <span> SixerDev</span></a>. All right Reserved</p>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="payment">
+              <a href="#"><img src="img/footer_payment_color.png" alt="payment"></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>`);
+});
