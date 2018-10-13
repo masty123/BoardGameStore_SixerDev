371bd8d22674ed5e5f04e99e07513a89ba6968a2
Pappim Pipatkasrira <printtopipatkasira@gmail.com>
2018-10-13 15:54:28 +0700

    Add secret admin registration route

--- routes/account.js
+++ routes/account.js
@@ -6,9 +6,70 @@ const passport = require('passport');
 //Bring in User model
 let User = require('../models/user');
 
+//Secret register form
+router.get('/register/secretAdmin3103', function(req, res){
+  res.render('register3103');
+});
+
 //Register form
 router.get('/register', function(req, res){
   res.render('register');
+});
+
+//Register as Admin Process
+router.post('/register/secretAdmin3103', function(req,res){
+  const name      = req.body.name;
+  const email     = req.body.email;
+  const username  = req.body.username;
+  const password  = req.body.password;
+  const password2 = req.body.password2;
+  const address   = req.body.address;
+  const secret   = req.body.secret;
+  const isAdmin   = true;
+
+  req.checkBody('name', 'Name is required').notEmpty();
+  req.checkBody('email', 'Email is required').notEmpty();
+  req.checkBody('email', 'Email is not valid').isEmail();
+  req.checkBody('username', 'Username is required').notEmpty();
+  req.checkBody('password', 'Password is required').notEmpty();
+  req.checkBody('password2', 'Confirm Password is not match').equals(password);
+  req.checkBody('address', 'Address is required').notEmpty();
+  req.checkBody('secret', 'Secret is required').notEmpty();
+
+  let errors = req.validationErrors();
+  if(errors || secret != "This is our admin secret"){
+    res.render('register3103',{
+      errors:errors
+    });
+  }
+  else{
+    let newUser = new User({
+      name:     name,
+      email:    email,
+      username: username,
+      password: password,
+      address:  address,
+      isAdmin:  isAdmin
+    });
+    bcrypt.genSalt(10, function(err,salt){
+      bcrypt.hash(newUser.password, salt, function(err, hash){
+        if(err){
+          console.log(err);
+        }
+        newUser.password = hash;
+        newUser.save(function(err){
+          if(err){
+            console.log(error);
+            return;
+          }
+          else{
+            req.flash('success','You are now registered and can be log in with account.');
+            res.redirect('/account/login');
+          }
+        })
+      });
+    });
+  }
 });
 
 //Register Process
