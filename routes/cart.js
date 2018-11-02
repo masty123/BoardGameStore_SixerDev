36050d59e273d49a0558589567d39f4e43929e3b
Pappim Pipatkasrira <printtopipatkasira@gmail.com>
2018-11-02 10:18:43 +0700

    Linked cart page

--- routes/cart.js
+++ routes/cart.js
@@ -2,14 +2,42 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const passport = require('passport');
+const mongoose = require('mongoose');
 
 let User = require('../models/user');
+let Product = require('../models/product');
 
 //Login Form
 router.get('/', function(req, res) {
-  res.render('cart', {
-    items: req.user.shopping_cart
-  });
+  if(req.user){
+    let products = [];
+    let price = 0;
+    let j = 0;
+    for (var i = 0 ; i < req.user.shopping_cart.length ; i++) {
+        Product.findById(req.user.shopping_cart[i], function(err, product) {
+          if (err) throw err;
+          if (!product) {
+            req.flash('warning', 'Your cart is empty');
+            res.redirect('/');
+          }
+          else {
+            products.push(product);
+            price += product.price;
+          }
+          if(j == req.user.shopping_cart.length - 1){
+            res.render('cart', {
+                products: products,
+                price: price
+            });
+          }
+          j++;
+        });
+    }
+  }
+  else{
+    req.flash('danger', 'Please log-in to use cart system');
+    res.redirect('/account/login');
+  }
 });
 
 router.post('/add/:id', function(req, res) {
